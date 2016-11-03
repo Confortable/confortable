b5888d068e22ad7ca947f66c2a31210b45d12837
Aurelien Giraud <aure@posteo.de>
2016-11-03 19:11:16 +0100

    Add Mocha testing for the Users' gravatar hook

--- test/services/user/hooks/gravatar.test.js
+++ test/services/user/hooks/gravatar.test.js
@@ -2,19 +2,24 @@
 
 const assert = require('assert');
 const gravatar = require('../../../../src/services/user/hooks/gravatar.js');
+const chai = require('chai');
+//use should
+var should = chai.should();
 
 describe('user gravatar hook', function() {
-  it('hook can be used', function() {
+  it('adds a string to the user\'s avatar key', function() {// does not make sure that this string is a URL
     const mockHook = {
       type: 'before',
       app: {},
       params: {},
       result: {},
-      data: {}
+      data: {
+        'email': 'user@email.com'
+      }
     };
-
     gravatar()(mockHook);
-
-    assert.ok(mockHook.gravatar);
+    mockHook.data.should.have.property('avatar');
+    mockHook.data.avatar.should.be.a('string');
+    mockHook.data.avatar.should.have.length.above(0);
   });
 });
